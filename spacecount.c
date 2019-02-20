ed60b071d23b6d86a95cced49fef66cf8a43d042
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-20 20:46:28 +0530

    Create spacecount.c

---
+++ spacecount.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{ 
+  int count=0;
+  char *a;
+  scanf("%[^\n]s",a);
+  for(int i=0;i<strlen(a);i++)
+  {
+    if(a[i]==' ')
+      count=count+1;
+    else
+     count+=0;  
+  }
+  printf("%d",count);
+  return 0;
+}
