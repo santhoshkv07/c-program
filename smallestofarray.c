dd94d1b201e8fac08206e0c43bf1d7e4f46941c1
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-20 19:02:01 +0530

    Create smallestofarray.c

---
+++ smallestofarray.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int array[50], size, i, smallest;
+    scanf("%d", &size);
+    for (i = 0; i < size; i++)
+		   scanf("%d", &array[i]);
+    smallest = array[0];
+    for (i = 1; i < size; i++) 
+  {
+		if (smallest > array[i])
+			smallest = array[i];
+	}
+ printf("%d",smallest);
+  return 0;
+}
