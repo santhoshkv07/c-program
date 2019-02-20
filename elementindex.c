ba3457c212ee8fb54017ab31e2d3645ec1bf1baa
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-20 19:46:31 +0530

    Update elementindex.c

--- elementindex.c
+++ elementindex.c
@@ -5,18 +5,6 @@ int main(void)
          scanf("%d", &n);
          for (i = 0; i < n; ++i)
             scanf("%d", &number[i]);
-         for (i = 0; i < n; ++i) 
-        {
-             for (j = i + 1; j < n; ++j)
-            {
-                 if (number[i] > number[j])
-                {
-                    a =  number[i];
-                    number[i] = number[j];
-                    number[j] = a;
-                 }
-             }
-         }
          for (i = 0; i < n; ++i)
             printf("%d %d\n", number[i],i);
   return 0;
