3215046002c76359acd222f944d8e09cc101f160
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-21 23:27:47 +0530

    Create norepeate.c

---
+++ norepeate.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,k,x[100],count=0;
+scanf("%d %d",&n,&k);
+for(int i=0;i<n;i++)
+{
+scanf("%d",&x[i]);
+}
+for(i=0;i<n;i++)
+{
+for(int j=i+1;j<n;j++)
+{
+if(n[i]==n[j])
+count++;
+}
+}
+printf("%d",count);
+return 0;
+}
