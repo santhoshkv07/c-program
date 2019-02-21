cdd845f1068e317412f874d5ad9a97c8b1f0fae9
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-21 22:37:49 +0530

    Create numberwords.c

---
+++ numberwords.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+ if(n=>1 && n<=10)
+ {
+   switch(n)
+   {
+    case 1:
+     printf("one");
+    case 2:
+      printf("two");
+    case 3:
+      printf("three");   
+    case 4:
+      printf("four");
+    case 5:
+      printf("five");
+    case 6:
+      printf("six");
+    case 7:
+      printf("seven");
+    case 8:
+      printf("eight");
+    case 9:
+      printf("nine");
+    case 10:
+      printf("ten");
+  }
+ }                      
+  return 0;
+}
