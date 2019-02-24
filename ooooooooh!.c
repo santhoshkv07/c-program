474e0a5870e842dc00d0f293b1a6c36bf69732bd
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-24 17:52:21 +0530

    Create ooooooooh!.c

---
+++ ooooooooh!.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int n1,n2,i;
+	char sym;
+  for(i=1;i<=4;i++)
+	{
+		scanf("%d %c %d",&n1,&sym,&n2);
+		if(sym=='/')
+		{
+			printf("%d\n",n1/n2);
+		}
+		else
+		{
+			printf("%d\n",n1%n2);
+		}
+	}
+	return 0;
+}
