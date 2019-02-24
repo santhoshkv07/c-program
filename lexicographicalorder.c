33e6271af2aeb28634f0b9314d1bb25c80e3e62b
santhoshkv07 <37010258+santhoshkv07@users.noreply.github.com>
2019-02-24 17:48:56 +0530

    Update lexicographicalorder.c

--- lexicographicalorder.c
+++ lexicographicalorder.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+int main() 
+{
+	char str[50],temp[50];
+	int i,k,j;
+	scanf("%[^\n]s",str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		for(j=i+1;str[j]!='\0';j++)
+		{
+			if(str[i]>str[j])
+			{
+				temp[k]=str[i];
+				str[i]=str[j];
+				str[j]=temp[k];
+		}	}
+	}
+	printf("\n%s",str);
+	return 0;
+}
